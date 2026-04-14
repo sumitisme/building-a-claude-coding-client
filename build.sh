9076db0a7a37a1af112dcc91ac936d93caad713b
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-04-14 06:24:10 +0545

    shell bugs

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 # I'm not super good with shell scripts.
 # It's not finding the root file. I need to fix this else nth works. Will fix it later.
 
-set -e # stop if any errors
+#!/bin/bash
 
+set -e # stop if any errors
 
 cd "$(dirname "$0")" # This ensures that the stuff is ran at the directory where this script lives
 
 # making a "build" file and determining the location of the vcpkg root
 # use $env:VCPKG_ROOT = "the location in your PC" manually to set up the vcpkg root
 
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake -G "Unix Makefiles" -B build -S . -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
 
 # generating and executing build
 cmake --build ./build
 
-exec $(dirname "$0")/build/claude-code "$@"
+./build/claude-code "$@"

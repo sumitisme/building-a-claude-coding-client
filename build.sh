19f487f582602dd56ac0b4e013d28bc9f741ad4b
folded epoxy <079bel090.sumit@pcampus.edu.np>
2026-04-16 22:18:20 +0545

    trying out other random shit to make this work

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e # stop if any errors
 
-cd "$(dirname "$0")" # This ensures that the stuff is ran at the directory where this script lives
+{
+    cd "$(dirname "$0")" # This ensures that the stuff is ran at the directory where this script lives
 
-# making a "build" file and determining the location of the vcpkg root
-# use $env:VCPKG_ROOT = "the location in your PC" manually to set up the vcpkg root
+    # making a "build" file and determining the location of the vcpkg root
+    # use $env:VCPKG_ROOT = "the location in your PC" manually to set up the vcpkg root
 
-cmake -G "Unix Makefiles" -B build -S . -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake"
+    # this is to force my system to use vcpkg since it isn't working normally.
+    cmake -G "Unix Makefiles" -B build -S . \
+        -DCMAKE_TOOLCHAIN_FILE="$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake" \
+        -DVCPKG_TARGET_TRIPLET=x64-mingw-static \
+        -DCMAKE_MAKE_PROGRAM=mingw32-make
 
-# generating and executing build
-cmake --build ./build
+    # generating and executing build
+    cmake --build ./build
+}
 
-./build/claude-code "$@"
+exec $(dirname "$0")/build/claude-code "$@"
 
 # I suck at shell scripts
